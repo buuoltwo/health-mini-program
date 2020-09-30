a4b43df1141f1f869d3b4b57133cc411195f872d
conceit-sz <bullnick20@gmail.com>
2020-09-30 22:37:16 +0800

    tabBar

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -11,18 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
     list:[{
-      pagePath: "/pages/home",
+      pagePath: "/pages/home/home",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "首页"
     },{
-      pagePath: "/pages/food",
-      iconPath: "/image/icon_component.png",
-      selectedIconPath: "/image/icon_component_HL.png",
+      pagePath: "/pages/food/food",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
       text: "食疗坊"
     },{
-      pagePath: "/pages/about",
+      pagePath: "/pages/about/about",
       iconPath: "/image/icon_component.png",
       selectedIconPath: "/image/icon_component_HL.png",
       text: "关于"
@@ -33,6 +36,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    switchTab(e) {
+      // console.log(e)
+      const data = e.currentTarget.dataset
+      wx.switchTab({
+        url: data.path,
+      })
+      this.setData({
+        selected: data.index
+      })
+    }
   }
 })
