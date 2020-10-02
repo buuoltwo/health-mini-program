3bfa1f3b9f20d395752a7cd54a36e17da5e5ec4f
conceit-sz <bullnick20@gmail.com>
2020-10-02 16:34:46 +0800

    模板-定义(给data写注释)-引用模板import-使用模板(is, data)-请求-block/for-data改item-定义模板改item

--- pages/food/food.js
+++ pages/food/food.js
@@ -5,14 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    foodArray: [],
+    items: {
+      foodlist: {
+        title:'xx',
+        image:'',
+        desc:"xx",
+        readNum:2333
+      }
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '食疗坊列表渲染中',
+    })
+    wx.showNavigationBarLoading()
+    wx.request({
+      url: 'http://iwenwiki.com:3002/api/foods/list',
+      data: {
+        city: '北京',
+        page: 1
+      },
+       success: res => {
+         console.log(res)
+         if(res.data.status === 200) {
+          const foodArray = res.data.data.result
+          this.setData({
+            foodArray
+          })
+         }
 
+       },
+        complete: () => {
+          wx.hideLoading()
+          wx.hideNavigationBarLoading()
+        }
+    })
   },
 
   /**
